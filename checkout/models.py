05eb3c05765b6618f205fa85225cfdb676c33e5b
Brianconn71 <Brian.connolly71@gmail.com>
2021-06-17 16:02:27 +0000

    set up methods in order class

--- checkout/models.py
+++ checkout/models.py
@@ -25,6 +25,18 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+        """
+            Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+    
+    def save(self, *args, **kwargs):
+        """
+            Override the original save method to set the order number
+            if it hasn't been set already
+        """
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
