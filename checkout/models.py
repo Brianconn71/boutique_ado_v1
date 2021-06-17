6b99228756f3518e8b6d6221151ea416089e17ce
Brianconn71 <Brian.connolly71@gmail.com>
2021-06-17 15:57:40 +0000

    set up checkout model

---
+++ checkout/models.py
@@ -0,0 +1,34 @@
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
+
+
+# Create your models here.
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, editable=False)
+    phone_number = models.CharField(max_length=20, null=False, editable=False)
+    country = models.CharField(max_length=40, null=False, editable=False)
+    postcode = models.CharField(max_length=20, null=True, editable=True)
+    town_or_city = models.CharField(max_length=40, null=False, editable=False)
+    street_address1 = models.Charfield(max_length=80, null=False, blank=False)
+    street_address2 = models.Charfield(max_length=80, null=True, blank=True)
+    county = models.Charfield(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True) #xs m xl
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
